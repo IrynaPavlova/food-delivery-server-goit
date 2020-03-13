c0241c7b2ba80eda3268bbc62c2436e60f7c2383
Pavlova <irochkapavlova85@gmail.com>
2020-03-13 16:20:22 +0200

    ready

--- src/routes/products/productRoute.js
+++ src/routes/products/productRoute.js
@@ -2,7 +2,8 @@ const url = require("url");
 
 const getAllProducts = require("./getAllProducts");
 const getProductById = require("./getProductById");
-const getProducByCategory = require("./getProductByCategory");
+const getProductByCategory = require("./getProductByCategory");
+const getProductByIds = require("./getProductsByIds");
 
 const productRoute = (request, response) => {
   if (request.method === "GET") {
@@ -10,11 +11,13 @@ const productRoute = (request, response) => {
     const path = parsedUrl.path;
 
     const pathId = /products\/\d/;
+    const pathIds = /products\/\?ids=[\d\D]/;
     const pathCategory = /products\/\?category=[\d\D]/;
 
     path === "/products" && getAllProducts(request, response);
     path.match(pathId) && getProductById(request, response);
-    path.match(pathCategory) && getProducByCategory(request, response);
+    path.match(pathIds) && getProductByIds(request, response);
+    path.match(pathCategory) && getProductByCategory(request, response);
 
     return;
   }
